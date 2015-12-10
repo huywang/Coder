b55412d117e188d1319c694fc03a2c6ae7713673
wang200804 <wang200804@126.com>
2015-12-10 18:20:43 +0800

    gcoder unittest modified

--- GCoder_unittest/CoderTester.cpp
+++ GCoder_unittest/CoderTester.cpp
@@ -1,8 +1,14 @@
 #include "gtest\gtest.h"
+#include "FileParser.h"
 
-class CoderTester : public testing::Test {
+class FileParserTester : public testing::Test {
 
 };
+
+TEST_F(FileParserTester, BasicTest) {
+  Coder::FileParser file_parser(L"data.txt");
+  EXPECT_EQ(file_parser.Parse(), true);
+}
 
 int main(int argc, char** argv) {
   RUN_ALL_TESTS();
